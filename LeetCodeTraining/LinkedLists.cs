ac1eb32246f96f3fb3f6bbeb5022d429d53bf572
Davi Oliveira <novaes.davi1@gmail.com>
2025-01-06 22:58:47 -0300

    Josephus problem in group

--- LeetCodeTraining/LinkedLists.cs
+++ LeetCodeTraining/LinkedLists.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class LinkedLists {
 
     public void ExecuteJosephusProblem () {
@@ -13,10 +15,30 @@ public class LinkedLists {
         }
 
         // Resolver o problema de Josephus
-        // Console.WriteLine ("Resolvendo o problema de Josephus...");
-        // int survivor = list.SolveJosephus (step);
+        Console.WriteLine ("Resolvendo o problema de Josephus...");
+        int survivor = list.SolveJosephus (step);
 
-        // Console.WriteLine ($"\nA pessoa que sobreviveu está na posição: {survivor}");
+        Console.WriteLine ($"\nA pessoa que sobreviveu está na posição: {survivor}");
+
+    }
+
+    public void ExecuteJosephusProblemGroup () {
+        JosephusProblemGroup list = new JosephusProblemGroup ();
+
+        // Número de pessoas no círculo
+        int n = 7; // Exemplo com 7 pessoas
+        int step = 3; // Exclui a cada 3ª pessoa
+
+        // Criar o círculo
+        for (int i = 1; i <= n; i++) {
+            list.Add (i);
+        }
+
+        // Resolver o problema de Josephus
+        Console.WriteLine ("Resolvendo o problema de Josephus...");
+        int survivor = list.SolveJosephus (step);
+
+        Console.WriteLine ($"\nA pessoa que sobreviveu está na posição: {survivor}");
 
     }
 
